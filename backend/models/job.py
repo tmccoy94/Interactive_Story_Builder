17bb9a741662ddaeed4a31efc4dcc9b7e00019cb
Taylor McCoy <mccoyt57@gmail.com>
2025-09-08 12:14:07 -0500

    created storyjob data model

---
+++ backend/models/job.py
@@ -0,0 +1,24 @@
+# This file is intended to represent the intent to make a story
+# so it works soemthing like frontend sends a job, backend starts on it. 
+# Front end asks if job is done, backend reports status
+# If job is done, backend can send story
+
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, JSON
+from sqlalchemy.sql import func
+
+from db.database import Base
+
+class StoryJob(Base):
+    __tablename__ = "story_jobs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    job_id = Column(String, index=True, unique=True)
+    session_id = Column(String, index=True)
+    theme = Column(String)
+    status = Column(String)
+    story_id = Column(Integer, nullable=True)
+    error = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    completed_job = Column(DateTime(timezone=True), nullable=True)
+    
+    
