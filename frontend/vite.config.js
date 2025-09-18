3c54ba6ed4e2af4f2c7ca114872fa9521eb00123
Taylor McCoy <mccoyt57@gmail.com>
2025-09-18 13:01:29 -0500

    updated config and requirements for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false
+export default defineConfig((command, mode) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  console.log(env.VITE_DEBUG)
+
+  return {
+    plugins: [react()],
+    server: {
+      ...(env.VITE_DEBUG === "true" && {
+        proxy: {
+          '/api': {
+            target: 'http://127.0.0.1:8000',
+            changeOrigin: true,
+            secure: false
+          }
+        }
       }
+      )
     }
   }
 })
